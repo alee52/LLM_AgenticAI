fd2e4b75dd73862634b661643de45ac7dc0b5242
Arum Lee <axl5699@psu.edu>
2026-04-25 16:52:42 -0400

    add data preps

---
+++ data_prep/items.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from datasets import Dataset, DatasetDict, load_dataset
+from typing import Optional, Self
+
+
+PREFIX = "Category is "
+top_level_list = {'Bundles', 'Food, Beverages & Tobacco', 'Product Add-Ons', 'Gift Cards', 'Hardware', 'Home & Garden', 'Sporting Goods', 'Electronics', 'Baby & Toddler', 'Uncategorized', 'Apparel & Accessories', 'Furniture', 'Media', 'Toys & Games', 'Religious & Ceremonial', 'Luggage & Bags', 'Cameras & Optics', 'Arts & Entertainment', 'Software', 'Office Supplies', 'Animals & Pet Supplies', 'Vehicles & Parts', 'Health & Beauty', 'Business & Industrial', 'Services'}
+QUESTION = "What is the category of the following product out of the following categories: {categories}".format(categories=", ".join(top_level_list))
+
+class Item(BaseModel):
+    """
+    An Item is a data-point of a Product with a category label. It can be used to create prompts for training a model to predict the category of a product based on its title and description.
+    """
+
+    title: str
+    category: str
+    full: Optional[str] = None
+    summary: Optional[str] = None
+    prompt: Optional[str] = None
+    id: Optional[int] = None
+
+    def make_prompt(self, text: str):
+        self.prompt = f"{QUESTION}\n\n{text}\n\n{PREFIX}{self.category}"
+
+    def test_prompt(self) -> str:
+        return self.prompt.split(PREFIX)[0] + PREFIX
+
+    def __repr__(self) -> str:
+        return f"<{self.title} = ${self.category}>"
+
+    @staticmethod
+    def push_to_hub(dataset_name: str, train: list[Self], val: list[Self], test: list[Self]):
+        """Push Item lists to HuggingFace Hub"""
+        DatasetDict(
+            {
+                "train": Dataset.from_list([item.model_dump() for item in train]),
+                "validation": Dataset.from_list([item.model_dump() for item in val]),
+                "test": Dataset.from_list([item.model_dump() for item in test]),
+            }
+        ).push_to_hub(dataset_name)
+
+    @classmethod
+    def from_hub(cls, dataset_name: str) -> tuple[list[Self], list[Self], list[Self]]:
+        """Load from HuggingFace Hub and reconstruct Items"""
+        ds = load_dataset(dataset_name)
+        return (
+            [cls.model_validate(row) for row in ds["train"]],
+            [cls.model_validate(row) for row in ds["validation"]],
+            [cls.model_validate(row) for row in ds["test"]],
+        )
